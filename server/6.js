7165f30e6cfcb33147ce4b6c0c90f037a90d30b9
biao <iambillzhao@gmail.com>
2016-03-05 12:03:21 +0800

    init demo page

---
+++ server/6.js
@@ -0,0 +1,96 @@
+[
+    {
+        "month": "6",
+        "status": [{
+            "day": "1",
+            "isSigned": "N"
+        },{
+            "day": "2",
+            "isSigned": "Y"
+        },{
+            "day": "3",
+            "isSigned": "N"
+        },{
+            "day": "4",
+            "isSigned": "N"
+        },{
+            "day": "5",
+            "isSigned": "N"
+        },{
+            "day": "6",
+            "isSigned": "N"
+        },{
+            "day": "7",
+            "isSigned": "Y"
+        },{
+            "day": "8",
+            "isSigned": "N"
+        },{
+            "day": "9",
+            "isSigned": "N"
+        },{
+            "day": "10",
+            "isSigned": "N"
+        },{
+            "day": "11",
+            "isSigned": "N"
+        },{
+            "day": "12",
+            "isSigned": "N"
+        },{
+            "day": "13",
+            "isSigned": "N"
+        },{
+            "day": "14",
+            "isSigned": "N"
+        },{
+            "day": "15",
+            "isSigned": "N"
+        },{
+            "day": "16",
+            "isSigned": "N"
+        },{
+            "day": "17",
+            "isSigned": "N"
+        },{
+            "day": "18",
+            "isSigned": "N"
+        },{
+            "day": "19",
+            "isSigned": "N"
+        },{
+            "day": "20",
+            "isSigned": "N"
+        },{
+            "day": "21",
+            "isSigned": "N"
+        },{
+            "day": "22",
+            "isSigned": "N"
+        },{
+            "day": "23",
+            "isSigned": "N"
+        },{
+            "day": "24",
+            "isSigned": "N"
+        },{
+            "day": "25",
+            "isSigned": "N"
+        },{
+            "day": "26",
+            "isSigned": "N"
+        },{
+            "day": "27",
+            "isSigned": "N"
+        },{
+            "day": "28",
+            "isSigned": "Y"
+        },{
+            "day": "29",
+            "isSigned": "N"
+        },{
+            "day": "30",
+            "isSigned": "N"
+        }]
+    }
+]
